a9675234ecd85af4703e8934712e44163718e153
Sirui Wang <sw2449@cornell.edu>
2025-09-21 14:17:23 -0400

    Update test.py

--- Lab 2/test.py
+++ Lab 2/test.py
@@ -6,16 +6,24 @@ from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 from time import strftime
 
+
+# Load the background image
+image1 = Image.open("clock_img/04.png")
+
+
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.D5) 
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = None
 
+
 # Config for display baudrate (default max is 24mhz):
 BAUDRATE = 64000000
 
+
 # Setup SPI bus using hardware SPI:
 spi = board.SPI()
+
 
 # Create the ST7789 display:
 disp = st7789.ST7789(
@@ -30,6 +38,7 @@ disp = st7789.ST7789(
     y_offset=40,
 )
 
+
 # Create blank image for drawing.
 # Make sure to create image with mode 'RGB' for full color.
 height = disp.width  # we swap height/width to rotate it to landscape!
@@ -37,12 +46,20 @@ width = disp.height
 image = Image.new("RGB", (width, height))
 rotation = 90
 
+
+# Resize the loaded image to fit the display
+image1 = image1.resize((width, height))
+
+
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
+
 
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 disp.image(image, rotation)
+
+
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = -2
@@ -51,27 +68,36 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
+
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 36)
+
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    # Use the loaded image as background instead of black rectangle
+    image.paste(image1, (0, 0))
+    
+    # Get drawing object (need to recreate after pasting image)
+    draw = ImageDraw.Draw(image)
+
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-    current_time = strftime("%m/%d/%Y %H:%M:%S")
-    lyrics = "sample text"
+    current_time = strftime("%M  %S")
     y = top
-    draw.text((x, y), current_time, font=font, fill="#FFFFFF")
-    draw.text((x, y+25), lyrics, font=font, fill="#FFFFFF")
+    draw.text((x, y), current_time, font=font, fill="#000000")
+
 
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
+
+
+
