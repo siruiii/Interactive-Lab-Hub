c5c501618bc1626a46029777e3b7b0cddc0db02a
siruiii <sw5546@nyu.edu>
2025-09-21 14:25:42 -0400

    Upload clock resource

--- Lab 2/test.py
+++ Lab 2/test.py
@@ -72,7 +72,8 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 36)
+# font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 36)
+font = ImageFont.truetype("proj_docs/Abel-Regular.ttf", 36)
 
 
 # Turn on the backlight
